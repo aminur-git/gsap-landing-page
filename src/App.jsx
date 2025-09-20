8cbaeb80cd1b420c3e2f867212df0ea277568b2e
aminur-git <aminur.4mi@gmail.com>
2025-09-21 02:56:31 +0600

    navbar, hero and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]"><h1>Hellow World!</h1></div>
+        <main>
+            <Navbar />
+            <Hero />
+            <div className="h-dvh bg-black">
+
+            </div>
+        </main>
     )
 }
 
