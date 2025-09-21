860b15c16b2e04267b196f9691ea0e572b3a983b
aminur-git <aminur.4mi@gmail.com>
2025-09-21 13:32:00 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import gsap from "gsap"
-import { ScrollTrigger, SplitText } from "gsap/all"
+import {ScrollTrigger, SplitText} from "gsap/all"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
+import Footer from "./components/footer.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
     return (
         <main>
-            <Navbar />
-            <Hero />
-            <Cocktails />
-            <About />
+            <Navbar/>
+            <Hero/>
+            <Cocktails/>
+            <About/>
+            <Footer/>
         </main>
     )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className={'text-center py-5 bg-purple-950'}>
+            Developed by Aminur Rahman
+        </footer>
+    )
+}
+export default Footer
